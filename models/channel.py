a951543f74fe9b857e4a1fcf3044dbdbedc6f911
Jatin Lal <jatinlal1994@gmail.com>
2024-09-29 12:10:24 +0100

    refactored models and added docstrings

--- models/channel.py
+++ models/channel.py
@@ -1,17 +1,25 @@
-from mongoengine import Document, StringField, DateTimeField, DictField, ReferenceField, ListField
+"""Database models for channel"""
+
+from uuid import uuid4
 from datetime import datetime, timezone
-import uuid
+
+from mongoengine import (
+    Document,
+    StringField,
+    DateTimeField,
+    DictField,
+    ReferenceField,
+    ListField
+)
+
 
 class Channel(Document):
-    id = StringField(default=lambda: str(uuid.uuid4()), primary_key=True)
+    """Channel for communication between teacher and assistants"""
+
+    id = StringField(default=lambda: str(uuid4()), primary_key=True)
     name = StringField(required=True, choices=["telegram", "whatsapp", "facebook", "instagram"])
     profile = DictField(required=True)
     teacher = ReferenceField('Teacher', required=True)
     assistants = ListField(ReferenceField('Assistant', required=False))
     created_at = DateTimeField(default=datetime.now(timezone.utc))
     updated_at = DateTimeField(default=datetime.now(timezone.utc))
-    meta = {
-        'indexes': [
-          
-        ]
-    }
--- models/teacher.py
+++ models/teacher.py
@@ -1,17 +1,35 @@
-from mongoengine import Document, StringField, DateTimeField, ReferenceField, ListField, EmbeddedDocumentField, EmbeddedDocument
+"""Database models for teachers"""
+
 from datetime import datetime, timezone
+
+from mongoengine import (
+    Document,
+    StringField,
+    DateTimeField,
+    ReferenceField,
+    ListField,
+    EmbeddedDocumentField,
+    EmbeddedDocument
+)
+
 from models.channel import Channel
 
+
 class Channels(EmbeddedDocument):
+    """Social media channels associated with a teacher."""
+
     telegram = ListField(ReferenceField(Channel, required=False))
     whatsapp = ListField(ReferenceField(Channel, required=False))
     facebook = ListField(ReferenceField(Channel, required=False))
     instagram = ListField(ReferenceField(Channel, required=False))
 
+
 class Teacher(Document):
+    """Details of teacher with associated information of social media channels."""
+
+    name = StringField(required=True)
     google_id = StringField(required=True, unique=True)
     email = StringField(required=True, unique=True)
-    name = StringField(required=True)
     profile_picture = StringField()
     channels = EmbeddedDocumentField(Channels, default=Channels)
     created_at = DateTimeField(default=datetime.now(timezone.utc))
